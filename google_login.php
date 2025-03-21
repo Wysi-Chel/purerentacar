30771c12b313e0c79a103c1a4533427095d74132
unknown <iam.chel1021@gmail.com>
2025-03-21 21:08:40 +0800

    removed google secret

--- google_login.php
+++ google_login.php
@@ -3,9 +3,9 @@ session_start();
 require_once 'vendor/autoload.php'; 
 
 $client = new Google_Client();
-$client->setClientId('512750139884-b2j0vkka61rqcba3gsjku83ulqnqkmmh.apps.googleusercontent.com');
-$client->setClientSecret('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');
-$client->setRedirectUri('http://localhost/purerentacar/google_callback.php');
+$client->setClientId('');
+$client->setClientSecret('');
+$client->setRedirectUri('');
 $client->addScope("email");
 $client->addScope("profile");
 
